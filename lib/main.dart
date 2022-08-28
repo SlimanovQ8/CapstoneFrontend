25c818a4abbc6fedbb22c29028fd6e2832c0c867
MajedALRamdan <majooddark237514@gmail.com>
2022-08-28 20:36:51 +0300

    charity-first-page crad remaining

--- lib/main.dart
+++ lib/main.dart
@@ -3,13 +3,14 @@ import 'package:go_router/go_router.dart';
 import 'package:tbr3at/pages/HomePage.dart';
 import 'package:tbr3at/pages/LoginPage.dart';
 import 'package:tbr3at/pages/SignUpPage.dart';
+import 'package:tbr3at/pages/CharityPage.dart';
 
 void main() {
-  runApp( MyApp());
+  runApp(MyApp());
 }
 
 class MyApp extends StatelessWidget {
-   MyApp({Key? key}) : super(key: key);
+  MyApp({Key? key}) : super(key: key);
 
   // This widget is the root of your application.
   @override
@@ -18,17 +19,13 @@ class MyApp extends StatelessWidget {
       debugShowCheckedModeBanner: false,
       routeInformationParser: _router.routeInformationParser,
       routerDelegate: _router.routerDelegate,
-
     );
   }
 
   //AuthProvider ap = AuthProvider();
-  final _router =
-  GoRouter(initialLocation: 1 == 2 ? "/signin" : "/", routes: [
-    GoRoute(path: "/", builder: (context, state) => SignUpPage()),
+  final _router = GoRouter(initialLocation: 1 == 2 ? "/signin" : "/", routes: [
+    GoRoute(path: "/", builder: (context, state) => CharityPage()),
     GoRoute(path: "/signin", builder: (context, state) => LoginPage()),
     GoRoute(path: "/home", builder: (context, state) => HomePage()),
-
   ]);
 }
-
